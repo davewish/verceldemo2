06368b486250ca3a89f1d101c7160ca57c21a562
Dawit Tekle <dvwish9214@gmail.com>
2021-07-10 11:37:00 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Branh dev
+          i am done it successfully
         </p>
         <a
           className="App-link"
